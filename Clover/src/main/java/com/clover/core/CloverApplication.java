8e26b9d47b3d9df8aa0de07488bdfcd9f7d7cbc0
KiiraClover <Kiira@DESKTOP-1FD8PIT>
2018-09-07 10:22:38 +0200

    b

--- Clover/src/main/java/com/clover/core/CloverApplication.java
+++ Clover/src/main/java/com/clover/core/CloverApplication.java
@@ -1,13 +1,12 @@
 package com.clover.core;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan("com.clover.core.mapper	")
+//@MapperScan("com.clover.core.mapper	")
 public class CloverApplication extends SpringBootServletInitializer
 {
 
